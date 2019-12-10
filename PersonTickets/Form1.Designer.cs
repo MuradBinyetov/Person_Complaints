d75fb71023b744842ebe4fdfd12fbd986a4fc73a
MuradBinyetov <murad_binyetov@mail.ru>
2019-12-10 13:00:50 +0400

    10.12.2019

---
+++ PersonTickets/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace PersonTickets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_touch = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtgrd = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_touch
+            // 
+            this.btn_touch.Location = new System.Drawing.Point(95, 23);
+            this.btn_touch.Name = "btn_touch";
+            this.btn_touch.Size = new System.Drawing.Size(75, 23);
+            this.btn_touch.TabIndex = 0;
+            this.btn_touch.Text = "Touch";
+            this.btn_touch.UseVisualStyleBackColor = true;
+            this.btn_touch.Click += new System.EventHandler(this.btn_touch_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(95, 119);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "People";
+            // 
+            // dtgrd
+            // 
+            this.dtgrd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrd.Location = new System.Drawing.Point(95, 192);
+            this.dtgrd.Name = "dtgrd";
+            this.dtgrd.Size = new System.Drawing.Size(486, 171);
+            this.dtgrd.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 450);
+            this.Controls.Add(this.dtgrd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_touch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_touch;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dtgrd;
+    }
+}
+
---
+++ PersonTickets/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonTickets
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_touch_Click(object sender, EventArgs e)
+        {
+
+            PersonDbEntities personDb = new PersonDbEntities();
+            
+          
+
+            comboBox1.DataSource = personDb.People.ToList();
+            
+
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "Id";
+
+
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            PersonDbEntities personDb = new PersonDbEntities();
+
+            string num= comboBox1.SelectedValue.ToString();
+
+            int a= Convert.ToInt32(num);
+            dtgrd.DataSource = personDb.Tickets.Where(y=>y.PersonId==a).Select(x => new { x.Person.Name, x.Person.Surname, x.Title, x.Description }).ToArray();
+
+            
+        }
+    }
+}
